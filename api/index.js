1bf523b87bbd8eb9a6e90e1151519ad78f327293
shresthapr <shresthaprajan38@gmail.com>
2021-01-15 09:21:53 +0200

    Add vercel configuration

--- api/index.js
+++ api/index.js
@@ -8,6 +8,16 @@ app.get('/api', (req, res) => {
     message: 'Hello from NodeJS'
   })
 })
+app.get('/test', (req, res) => {
+  res.json({
+    message: 'Hello from Test'
+  })
+})
+app.get('/api/test', (req, res) => {
+  res.json({
+    message: 'Hello from api/test'
+  })
+})
 
 
 
